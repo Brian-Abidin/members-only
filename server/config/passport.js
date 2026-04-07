e6666fb022dd1c6ad73e5f4a14456419286533cc
Brian Abidin <Brian.abidin1@gmail.com>
2026-04-07 15:54:53 -0400

    renamed encryptPassword to createUser and updated the function

--- server/config/passport.js
+++ server/config/passport.js
@@ -70,13 +70,18 @@ const logoutRequest = (req, res, next) => {
   });
 };
 
-const encryptPassword = async (req, res, next) => {
+const createUser = async (req, res, next) => {
   try {
     const hashedPassword = await bcrypt.hash(req.body.password, 10);
-    await pool.query("INSERT INTO users (username, password) VALUES ($1, $2)", [
-      req.body.username,
-      hashedPassword
-    ]);
+    await pool.query(
+      "INSERT INTO users (username, password, first_name, last_name) VALUES ($1, $2, $3, $4)",
+      [
+        req.body.username,
+        hashedPassword,
+        req.body["first-name"],
+        req.body["last-name"]
+      ]
+    );
     res.redirect("/");
   } catch (error) {
     console.error(error);
@@ -91,5 +96,5 @@ module.exports = {
   currentUser,
   passportAuth,
   logoutRequest,
-  encryptPassword
+  createUser
 };
