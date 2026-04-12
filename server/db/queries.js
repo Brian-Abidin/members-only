a1d23f9ed9ac896d7c943a1fd4b244cb930cb181
Brian Abidin <Brian.abidin1@gmail.com>
2026-04-12 19:19:50 -0400

    fixed query inside insertMessage

--- server/db/queries.js
+++ server/db/queries.js
@@ -2,7 +2,7 @@ const pool = require("./pool");
 
 async function insertMessage(author, title, message) {
   await pool.query(
-    "INSERT INTO messages (title, message, absolute_time, author_id) WITH t1 AS (SELECT id FROM users WHERE username = $1) SELECT $2, $3, now(), t1.id FROM t1;",
+    "INSERT INTO messages (title, message, absolute_time, author_id) WITH t1 AS (SELECT id FROM users WHERE username = $1) SELECT $2, $3, now(), t1.id FROM t1",
     [author, title, message]
   );
 }
