518fc7682a3eaa14bf8c70ea8ad4231ddf48b075
Brian Abidin <Brian.abidin1@gmail.com>
2026-04-24 14:45:01 -0400

    moved the handler functions to handlers.js

--- server/__tests__/handlers.test.js
+++ server/__tests__/handlers.test.js
@@ -1,7 +1,1 @@
 // test all function that are used to render the page
-
-exports.home = (req, res) => res.render("index");
-exports.signup = (req, res) => res.render("sign-up-form");
-exports.error = (req, res) => res.render("failure");
-exports.message = (req, res) => res.render("message");
-exports.membersForm = (req, res) => res.render("members-form");
