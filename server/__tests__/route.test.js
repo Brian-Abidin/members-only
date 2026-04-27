1868bb015668c964c6647e3a7ab57a9994c87a12
Brian Abidin <Brian.abidin1@gmail.com>
2026-04-26 21:15:52 -0400

    renamed handler.test.js to routes.test.js

---
+++ server/__tests__/route.test.js
@@ -0,0 +1,30 @@
+// test all function that are used to render the page
+const request = require("supertest");
+const handlers = require("../handlers");
+const app = require("../app");
+
+describe("routes", () => {
+  describe("GET /", () => {
+    it("should return 200 status and return to the home page", async () => {
+      const response = await request(app).get("/");
+
+      expect(response.statusCode).toBe(200);
+      expect(response.text).toMatchSnapshot();
+    });
+  });
+  describe("GET /sign-up", () => {
+    it("should return 200 status and return to the sign up page", async () => {
+      const response = await request(app).get("/sign-up");
+
+      expect(response.statusCode).toBe(200);
+      expect(response.text).toMatchSnapshot();
+    });
+  });
+  describe("GET unknown route", () => {
+    it("should return 404 for an unknown route", async () => {
+      const response = await request(app).get("/fakeroute");
+
+      expect(response.statusCode).toBe(404);
+    });
+  });
+});
