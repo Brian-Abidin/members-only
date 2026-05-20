19c72f4f3d2012b16f71aec31e3e1a2e26e4d134
Brian Abidin <Brian.abidin1@gmail.com>
2026-05-20 16:43:27 -0400

    added js functions for time passed

--- public/js/script.js
+++ public/js/script.js
@@ -1,5 +1,28 @@
-function findDate(messageDate) {
-  const date = new Date();
-  date.setDate(date.getDate() - messageDate);
-  return date.toDateString();
+import { getAllMessages } from "../../db/queries";
+
+console.log("helllooo");
+
+async function getAllMessagesDates() {
+  const messages = await getAllMessages();
+  const messagesDatesArr = messages.map((message) => message.absolute_time);
+  return messagesDatesArr;
+}
+
+function findTimePassed() {
+  const dates = getAllMessagesDates();
+  const now = new Date();
+  const timePassedArr = [];
+  for (let i = 0; i < dates.length; i += 1) {
+    const timePassed = (now - new Date(dates[i])) * (1000 * 60 * 60 * 24); // days
+    timePassedArr.push(timePassed);
+  }
+  console.log(timePassedArr, "DFSFDSf");
+  return timePassedArr;
+}
+
+const time = document.getElementsByClassName("time");
+const timePassedArr = findTimePassed();
+
+for (let i = 0; i < time.length; i += 1) {
+  time[i].textContent = timePassedArr[i];
 }
