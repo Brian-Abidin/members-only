23bec91f914ff702eb5d2881cd35d0defacae965
Brian Abidin <Brian.abidin1@gmail.com>
2026-05-29 14:39:57 -0400

    added scripts for toggling the header dropdown menu showing and hidden

--- public/js/script.js
+++ public/js/script.js
@@ -33,19 +33,45 @@ const deleteModal = document.getElementById("delete-modal");
 const closeBtn = document.getElementById("close-btn");
 const submitBtn = document.getElementById("submit-btn");
 
-function closeModal() {
-  console.log("hello");
-  if (deleteModal.style.display === "flex") {
-    deleteModal.style.display = "none";
+// header document elements
+const dropBtn = document.getElementById("drop-btn");
+const dropdown = document.getElementById("dropdown");
+
+function toggleDropdown() {
+  if (dropdown.style.display === "flex") {
+    dropdown.style.display = "none";
   } else {
-    deleteModal.style.display = "flex";
+    dropdown.style.display = "flex";
   }
 }
 
-deleteModal.addEventListener("click", (event) => {
-  if (deleteModal === event.target) deleteModal.style.display = "none";
-});
+if (dropBtn) {
+  dropBtn.addEventListener("click", toggleDropdown);
+}
+
+console.log(dropdown, "SDFSEFSF");
+
+if (dropBtn) {
+  window.addEventListener("click", (event) => {
+    if (dropBtn !== event.target) {
+      dropdown.style.display = "none";
+    }
+  });
+}
+
+function closeModal() {
+  console.log("hello");
+  if (deleteModal.style.display === "block") {
+    deleteModal.style.display = "none";
+  } else {
+    deleteModal.style.display = "block";
+  }
+}
 
 if (delButton) {
   delButton.addEventListener("click", closeModal);
+
+  deleteModal.addEventListener("click", (event) => {
+    if (deleteModal === event.target) deleteModal.style.display = "none";
+  });
 }
