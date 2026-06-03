ca506e6dedcfd68a21cd3c7a486bad67d2122056
Brian Abidin <Brian.abidin1@gmail.com>
2026-06-03 16:31:39 -0400

    updated getAllUsers function that gets all the user data instead of only the username

--- db/queries.js
+++ db/queries.js
@@ -1,7 +1,7 @@
 const pool = require("./pool");
 
 async function getAllUsers() {
-  const { rows } = await pool.query("SELECT username FROM users");
+  const { rows } = await pool.query("SELECT * FROM users");
   return rows;
 }
 
