a1dce16ab91bd62182b6a5191a90c8ca6e968cc1
Brian Abidin <Brian.abidin1@gmail.com>
2026-06-08 17:03:01 -0400

    removed commented out code

--- public/js/script.js
+++ public/js/script.js
@@ -1,33 +1,3 @@
-// import { getAllMessages } from "../../db/queries";
-
-// const time = document.getElementsByClassName("time");
-
-// console.log("helllooo");
-
-// async function getAllMessagesDates() {
-//   const messages = await getAllMessages();
-//   const messagesDatesArr = messages.map((message) => message.absolute_time);
-//   return messagesDatesArr;
-// }
-
-// function findTimePassed() {
-//   const dates = getAllMessagesDates();
-//   const now = new Date();
-//   const timePassedArr = [];
-//   for (let i = 0; i < dates.length; i += 1) {
-//     const timePassed = (now - new Date(dates[i])) * (1000 * 60 * 60 * 24); // days
-//     timePassedArr.push(timePassed);
-//   }
-//   console.log(timePassedArr, "DFSFDSf");
-//   return timePassedArr;
-// }
-
-// const timePassedArr = findTimePassed();
-
-// for (let i = 0; i < time.length; i += 1) {
-//   time[i].textContent = timePassedArr[i];
-// }
-
 const delButton = document.getElementById("delete-btn");
 const deleteModal = document.getElementById("delete-modal");
 const closeBtn = document.getElementById("close-btn");
